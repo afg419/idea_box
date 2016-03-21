6d46c679802695f52f9db121699a3519716f6607
Aaron Greenspan <afg419@gmail.com>
2016-03-21 17:08:12 -0600

    Implements basic index ui

--- spec/controllers/ideas_controller_spec.rb
+++ spec/controllers/ideas_controller_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe Api::V1::IdeasController, type: :controller do
     reply = JSON.parse(response.body)
 
     expect(reply.length).to eq 3
-    reply.each_index do |i|
+    reply.each_index do |i| #2-i because they are ordered by created at
       expect(reply[i]["title"]).to eq "Title#{2-i}"
       expect(reply[i]["body"]).to eq "Body#{2-i}"
       expect(reply[i]["quality"]).to eq 2-i
