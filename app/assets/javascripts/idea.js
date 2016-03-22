f9e794fee017a3538abe4cca9f68aea7003f9e46
Aaron Greenspan <afg419@gmail.com>
2016-03-22 12:49:25 -0600

    It is niccce

--- app/assets/javascripts/idea.js
+++ app/assets/javascripts/idea.js
@@ -4,110 +4,32 @@ class Idea{
     this.body = body;
     this.quality = quality;
     this.id = id;
+    this.format = new IdeaFormat(title, body, quality, id)
+    this.delete = new IdeaDelete(id)
+    this.voter = new IdeaVote(id)
   }
 
   render(){
-    $('.ideas').prepend(this.domIdea());
+    $('.ideas').prepend(this.ideaHtml());
     this.setEventListeners();
   }
 
   reRender(){
-    $('.ideas').find(`#idea-${this.id}`).replaceWith(this.domIdea());
+    $('.ideas').find(`#idea-${this.id}`).replaceWith(this.ideaHtml());
     this.setEventListeners();
   }
 
-  domIdea(){
+  ideaHtml(){
     return (`<li id="idea-${this.id}" class="idea">` +
-                this.voter() +
-                `<div class=''>${this.ideaTitle()}</div>` + ` ${this.formatQuality()}` +
-                `<p class=''>Content: ${this.ideaBody()}<\p>` +
-                this.deleteButton() +
+                this.voter.buttons() +
+                `<div class=''>${this.format.ideaTitle()}</div>` + ` ${this.format.ideaQuality()}` +
+                `<p class=''>Content: ${this.format.ideaBody()}<\p>` +
+                this.delete.button() +
             `</li>`)
   }
 
-  ideaTitle(){
-    return this.title
-  }
-
-  formatQuality(){
-    return {0: "swill", 1: "plausible", 2: "genius"}[this.quality]
-  }
-
-  ideaBody(){
-    var index = this.body.split(" ").reduce(function(acc, word){
-      if(acc + word.length < 100){
-        return acc + word.length
-      } else {
-        return acc
-      }
-    }, 0)
-    return this.body.substring(0,index)
-  }
-
-  deleteButton(){
-    return `<input id="delete-${this.id}" class="delete-idea" type="button" value="Delete">`
-  }
-
-  voter(){
-    return (`<input class="upvote" type="button" value="^">` +
-           `<input class="downvote" type="button" value="v">`)
-  }
-
-  setVoteListener(){
-    $(`#idea-${this.id}`).find('.upvote').on('click', () => vote(1,this.id))
-    $(`#idea-${this.id}`).find('.downvote').on('click', () => vote(-1,this.id))
-  };
-
-  setDeleteListener(){
-    $(`#delete-${this.id}`).on('click', () => {deleteIdea(this.id)})
-  }
-
   setEventListeners(){
-    this.setVoteListener();
-    this.setDeleteListener();
+    this.voter.setListener();
+    this.delete.setListener();
   }
 }
-
-
-
-
-
-// function renderIdea(idea){
-//   $('.ideas').prepend(domIdea(idea));
-//   setDeleteListener(idea.id);
-//   setVoteListener(idea.id)
-// }
-//
-
-//
-// function domIdea(idea){
-//   return (`<li id="idea-${idea.id}" class="idea">` +
-//               voter() +
-//               `<div class=''>${ideaTitle(idea)}</div>` + ` ${formatQuality(idea.quality)}` +
-//               `<p class=''>Content: ${ideaBody(idea)}<\p>` +
-//               deleteButton(idea.id) +
-//           `</li>`)
-// }
-//
-// function ideaTitle(idea){
-//   return idea.title
-// }
-//
-// function ideaBody(idea){
-//   return formatBody(idea.body)
-// }
-//
-// function formatQuality(quality){
-//   return {0: "swill", 1: "plausible", 2: "genius"}[quality]
-// }
-//
-// function formatBody(body){
-//   var index = body.split(" ").reduce(function(acc, word){
-//     if(acc + word.length < 100){
-//       return acc + word.length
-//     } else {
-//       return acc
-//     }
-//   }, 0)
-//   return body.substring(0,index)
-// }
