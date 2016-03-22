176ca94a3cfec18585375040e9901298ee9635a2
Aaron Greenspan <afg419@gmail.com>
2016-03-22 11:00:08 -0600

    Prepping for OO spike

--- app/assets/javascripts/idea.js
+++ app/assets/javascripts/idea.js
@@ -1,5 +1,32 @@
+// function Idea(){
+//   renderIdea(){
+//     $('.ideas').prepend(domIdea(this));
+//     setDeleteListener(this.id);
+//     setVoteListener(this.id)
+//   }
+//
+//   domIdea(){
+//     return (`<li id="idea-${this.id}" class="idea">` +
+//                 voter() +
+//                 `<div class=''>${ideaTitle(idea)}</div>` + ` ${formatQuality(idea.quality)}` +
+//                 `<p class=''>Content: ${ideaBody(idea)}<\p>` +
+//                 deleteButton(idea.id) +
+//             `</li>`)
+//   }
+// }
+
+
+
+
+
 function renderIdea(idea){
   $('.ideas').prepend(domIdea(idea));
+  setDeleteListener(idea.id);
+  setVoteListener(idea.id)
+}
+
+function reRenderIdea(idea){
+  $('.ideas').find(`#idea-${idea.id}`).replaceWith(domIdea(idea))
   setDeleteListener(idea.id);
   setVoteListener(idea.id)
 }
@@ -7,10 +34,18 @@ function renderIdea(idea){
 function domIdea(idea){
   return (`<li id="idea-${idea.id}" class="idea">` +
               voter() +
-              `  ${idea.title}: ${formatQuality(idea.quality)}` +
-              `<p>Content: ${formatBody(idea.body)}<\p>`+
+              `<div class=''>${ideaTitle(idea)}</div>` + ` ${formatQuality(idea.quality)}` +
+              `<p class=''>Content: ${ideaBody(idea)}<\p>` +
               deleteButton(idea.id) +
           `</li>`)
+}
+
+function ideaTitle(idea){
+  return idea.title
+}
+
+function ideaBody(idea){
+  return formatBody(idea.body)
 }
 
 function formatQuality(quality){
@@ -26,10 +61,4 @@ function formatBody(body){
     }
   }, 0)
   return body.substring(0,index)
-}
-
-function reRenderIdea(idea){
-  $('.ideas').find(`#idea-${idea.id}`).replaceWith(domIdea(idea))
-  setDeleteListener(idea.id);
-  setVoteListener(idea.id)
 }
