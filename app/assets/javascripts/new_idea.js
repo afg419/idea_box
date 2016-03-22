8441bee304bd7876080cfb502a10669d6ba9d775
Aaron Greenspan <afg419@gmail.com>
2016-03-21 18:38:42 -0600

    refactors new idea js a bit

--- app/assets/javascripts/new_idea.js
+++ app/assets/javascripts/new_idea.js
@@ -3,19 +3,28 @@ $(document).ready(function(){
 })
 
 function createIdea(){
-  var title = $('#new-idea-title').val()
-  var desc = $('#new-idea-desc').val()
-  var data = {idea: {title: title, body: desc, quality: 0}}
   $.ajax({
       type: "POST",
       url: "/api/v1/ideas",
-      data: data,
+      data: getNewIdeaFormData(),
       success: function(msg){
         renderIdea(msg);
+        wipeNewIdeaFields();
       },
       error: function(error_message){
-        alert(error_message.responseText)
-        console.log(error_message.responseText)
+        alert(error_message.responseText);
+        console.log(error_message.responseText);
       }
   });
+}
+
+function getNewIdeaFormData(){
+  var title = $('#new-idea-title').val()
+  var desc = $('#new-idea-desc').val()
+  return data = {idea: {title: title, body: desc, quality: 0}}
+}
+
+function wipeNewIdeaFields(){
+  $('#new-idea-title').val("")
+  $('#new-idea-desc').val("")
 }
